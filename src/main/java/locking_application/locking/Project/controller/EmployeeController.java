aed471e27a3ec78f1668e3024be3082be6c94b7f
aje <ajith@gmail.com>
2025-04-29 17:33:34 +0530

    controller layer addedd

--- src/main/java/locking_application/locking/Project/controller/EmployeeController.java
+++ src/main/java/locking_application/locking/Project/controller/EmployeeController.java
@@ -1,4 +1,26 @@
 package locking_application.locking.Project.controller;
 
+
+import locking_application.locking.Project.dto.PatientUpdateDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/optimize")
+@RestController
+@RequiredArgsConstructor
 public class EmployeeController {
+
+    private final PatientService patientService;
+
+    @PostMapping
+    public ResponseEntity<String> storeUserData(@RequestBody PatientUpdateDTO dto){
+
+    String userData=    patientService.storeUserData(dto);
+
+        return ResponseEntity.ok("Stored Successfull");
+    }
 }
--- src/main/java/locking_application/locking/Project/dto/PatientUpdateDTO.java
+++ src/main/java/locking_application/locking/Project/dto/PatientUpdateDTO.java
@@ -2,13 +2,17 @@ package locking_application.locking.Project.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Data
+
 public class PatientUpdateDTO {
     private String contactInfo;
+
     private String diagnosis;
 
 }
