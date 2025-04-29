d960c3c292b4fb1a6ea4a4f578a7feb129f65b5a
sukesh <sukeshkr8791@gmail.com>
2025-04-29 17:02:32 +0530

    we added address entity

---
+++ src/main/java/locking_application/locking/Project/entity/Address.java
@@ -0,0 +1,15 @@
+package locking_application.locking.Project.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Address {
+    private Integer id;
+    private String city;
+}
