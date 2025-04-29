8e88fe2c3158db87bad34d507824b132410ace0c
sukesh <sukeshkr8791@gmail.com>
2025-04-29 15:09:08 +0530

    sntity done

---
+++ src/main/java/locking_application/locking/Project/entity/DoctorEntity.java
@@ -0,0 +1,16 @@
+package locking_application.locking.Project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class DoctorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+}
