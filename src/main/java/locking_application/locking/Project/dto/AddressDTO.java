87cf2487392b3e5e21f20cec2de5c85054162326
ajithdev <ajithofficial2028@gmail.com>
2025-05-05 17:25:09 +0530

    3 annotations added

--- src/main/java/locking_application/locking/Project/dto/AddressDTO.java
+++ src/main/java/locking_application/locking/Project/dto/AddressDTO.java
@@ -1,5 +1,13 @@
 package locking_application.locking.Project.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AddressDTO {
     private Integer id;
     private String city;
