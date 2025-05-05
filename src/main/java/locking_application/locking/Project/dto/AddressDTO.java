82a5a0c97114e729019009a2098d5594d3f730ae
sukesh <sukeshkr8791@gmail.com>
2025-05-05 16:36:52 +0530

    remove dto class properties

--- src/main/java/locking_application/locking/Project/dto/AddressDTO.java
+++ src/main/java/locking_application/locking/Project/dto/AddressDTO.java
@@ -1,9 +1,5 @@
 package locking_application.locking.Project.dto;
 
 public class AddressDTO {
-    private Integer id;
-    private String city;
-    private String state;
-    private Long pincode;
-    private String country;
+
 }
